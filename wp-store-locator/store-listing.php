26b03624403119c61e6911b98bbd48e63b9da40b
zaghet <v.zaghet@twow.it>
2025-05-16 18:17:45 +0200

    store-listing.php per mostrare la descrizione dentro la lista a fianco alla mappa

---
+++ wp-store-locator/store-listing.php
@@ -0,0 +1,23 @@
+<?php
+global $wpsl_stores;
+
+if ( ! empty( $wpsl_stores ) ) : ?>
+    <ul class="wpsl-store-list">
+        <?php foreach ( $wpsl_stores as $store ) : 
+            $post_id = $store['id'];
+        ?>
+            <li class="wpsl-store" data-store-id="<?php echo esc_attr( $post_id ); ?>">
+                <strong class="store-name"><?php echo esc_html( $store['name'] ); ?></strong><br>
+                <?php echo esc_html( $store['address'] ); ?>, <?php echo esc_html( $store['city'] ); ?><br>
+
+                <?php if ( ! empty( $store['address2'] ) ) : ?>
+                    <p class="store-description" style="margin-top: 5px; font-size: 0.9em; color: #555;">
+                        <?php echo nl2br( esc_html( trim( $store['address2'] ) ) ); ?>
+                    </p>
+                <?php endif; ?>
+            </li>
+        <?php endforeach; ?>
+    </ul>
+<?php else : ?>
+    <p><?php esc_html_e( 'Nessun punto vendita trovato.', 'wp-store-locator' ); ?></p>
+<?php endif; ?>
